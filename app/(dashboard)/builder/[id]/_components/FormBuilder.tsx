a8985823b80903a2be535e62bb881b8ccbd7a1c5
Mirzaadr <mirzaadr34@yahoo.com>
2025-01-20 17:45:34 +0700

    edit text published

--- app/(dashboard)/builder/[id]/_components/FormBuilder.tsx
+++ app/(dashboard)/builder/[id]/_components/FormBuilder.tsx
@@ -57,7 +57,7 @@ const FormBuilder = ({ form }: FormBuilderProps) => {
         <div className="flex flex-col items-center justify-center h-full w-full">
           <div className="max-w-md">
             <h1 className="text-center text-4xl font-bold text-primary border-b pb-2 mb-10">
-              Form Published
+              🎊🎊 Form Published 🎊🎊
             </h1>
             <h2 className="text-2xl">Share this form</h2>
             <h3 className="text-xl text-muted-foreground border-b pb-10">
