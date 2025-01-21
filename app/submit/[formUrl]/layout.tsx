ab12b3cc4e3d210f010e8601aa09e50fa4fbdf9c
Mirzaadr <mirzaadr34@yahoo.com>
2025-01-21 21:05:43 +0700

    edit layout

--- app/submit/[formUrl]/layout.tsx
+++ app/submit/[formUrl]/layout.tsx
@@ -4,7 +4,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen h-screen">
       <Navbar />
-      <main className="flex flex-grow h-full w-full overflow-y-auto">
+      <main className="flex flex-grow w-full overflow-y-auto py-4 h-fit">
         {children}
       </main>
     </div>
